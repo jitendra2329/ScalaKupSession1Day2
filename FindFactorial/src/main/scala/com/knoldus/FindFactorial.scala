e919467df43f5e0e67164db4538f1d5d2e4d4152
Jitendra Kumar <124979608+jitendra2329@users.noreply.github.com>
2023-03-15 11:00:16 +0530

    Update FindFactorial.scala
    data type changed to BigInt

--- FindFactorial/src/main/scala/com/knoldus/FindFactorial.scala
+++ FindFactorial/src/main/scala/com/knoldus/FindFactorial.scala
@@ -4,7 +4,7 @@ import scala.annotation.tailrec
 class FindFactorial  {
 
   @tailrec
-  final def findFactorial(number: Int,accumulator: Long = 1): Long = {
+  final def findFactorial(number: Int,accumulator: BigInt = 1): BigInt = {
     if(number <= 1) accumulator else findFactorial(number - 1, number * accumulator)
 
   }
